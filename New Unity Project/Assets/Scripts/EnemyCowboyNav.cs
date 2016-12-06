f014ded458104dbff09e4920521571eaffbade1a
psmax <shmarov_max@mail.ru>
2016-12-06 20:55:17 +0300

    Ну поделал маленько, тыщ на 12

---
+++ New Unity Project/Assets/Scripts/EnemyCowboyNav.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCowboyNav : MonoBehaviour
+{
+    public Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    { 
+        agent = GetComponent<NavMeshAgent>();
+
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position); 
+    }
+
+}
+
---
+++ New Unity Project/Assets/Scripts/EnemyWalk.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyWalk : MonoBehaviour
+{
+
+    public GameObject Cowboy ;
+    public float timer;
+    public bool yes;
+    public float newtimer;
+
+
+    void Start()
+    {
+        newtimer = timer;
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Cowboy.GetComponent<NavMeshAgent>().enabled = true;
+            Cowboy.GetComponent<Animator>().SetTrigger("Walk");
+
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            yes = true;
+        }
+
+
+
+    }
+    void Update()
+    {
+        if (yes == true)
+        {
+            timer -= Time.deltaTime;
+        }
+        if (timer < 0)
+        {
+            Cowboy.GetComponent<NavMeshAgent>().enabled = false;
+            yes = false;
+            timer = newtimer;
+            Cowboy.GetComponent<Animator>().SetTrigger("Idle");
+        }
+
+    }
+}
+
+
+
