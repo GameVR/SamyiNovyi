7b39d6dff52503d773e163e207bfc40f0d267906
psmax <shmarov_max@mail.ru>
2016-12-07 10:37:04 +0300

    еще маленько

---
+++ New Unity Project/Assets/Scripts/EnemyCowboyAttack.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyCowboyAttack : MonoBehaviour
+{
+    public GameObject Cowboy;
+ //   public GameObject DeadZone;
+
+    void Start()
+    {
+   //     DeadZone.SetActive(false);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Cowboy.GetComponent<Animator>().SetTrigger("Idle");
+            Cowboy.GetComponent<NavMeshAgent>().enabled = false;
+            //    DeadZone.SetActive(true);
+
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Cowboy.GetComponent<Animator>().SetTrigger("Walk");
+            Cowboy.GetComponent<NavMeshAgent>().enabled = true;
+            //        DeadZone.SetActive(false);
+        }
+    }
+}
+
+
---
+++ New Unity Project/Assets/Scripts/SheriffAction.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SheriffAction : MonoBehaviour {
+
+    public Transform Player;
+    public Transform Sheriff;
+    public bool inVision;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inVision = true;
+            
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            inVision = false;
+        }
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (inVision)
+        {
+           
+        }
+        else
+        {
+   
+        }     
+        
+    }
+}
